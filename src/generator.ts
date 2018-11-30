3cfccc6feb901edc5feb68741f6ff7ae9f654a87
Matt Moriarity <matt@mattmoriarity.com>
2018-11-29 18:04:39 -0600

    Use nunjucks to render index template

--- src/generator.ts
+++ src/generator.ts
@@ -1,15 +1,29 @@
 import * as S3 from "aws-sdk/clients/s3";
+import * as nunjucks from "nunjucks";
 
 import * as site from "./model/site";
 
 const s3 = new S3();
 
-const indexHtml: string = "<!DOCTYPE html><html><head><title>BLOG_TITLE</title></head><body><h1>BLOG_TITLE</h1><p>This is where my content will go.</p></body></html>";
+nunjucks.configure({ autoescaping: true });
+
+const indexTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>{{ site.title }}</title>
+</head>
+<body>
+  <h1>{{ site.title }}</h1>
+  <p>This site was compiled with a Nunjucks template!</p>
+</body>
+</html>
+`
 
 export default async function generate(blogId: string): Promise<void> {
   const siteConfig = await site.getConfig(blogId);
 
-  const body = indexHtml.replace(/BLOG_TITLE/g, siteConfig.title);
+  const body = nunjucks.renderString(indexTemplate, { site: siteConfig });
 
   await s3.putObject({
     Bucket: blogId, // TODO use a key in the config for this
