584b93bd5bb54ea01d62d012496f4b606ca3db95
Matt Moriarity <matt@mattmoriarity.com>
2018-12-01 15:17:25 -0600

    Automatically generate the site when DB changes are made

--- src/functions/generateSite.ts
+++ src/functions/generateSite.ts
@@ -1,8 +1,16 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, DynamoDBStreamHandler } from 'aws-lambda';
 
 import generate from "../generate";
 
-export const handle: APIGatewayProxyHandler = async (event, context) => {
+export async function handle(event, context) {
+  if (event.Records) {
+    return await handleDynamoDBTrigger(event, context, undefined);
+  } else {
+    return await handleHttp(event, context, undefined);
+  }
+};
+
+const handleHttp: APIGatewayProxyHandler = async (event, context) => {
   const { blogId } = JSON.parse(event.body);
   await generate(blogId);
 
@@ -13,4 +21,18 @@ export const handle: APIGatewayProxyHandler = async (event, context) => {
       message: "Your website was generated successfully."
     })
   }
-}
+};
+
+const handleDynamoDBTrigger: DynamoDBStreamHandler = async (event, context) => {
+  let blogIds = new Set();
+
+  // collect the unique IDs of the blogs that were affected
+  event.Records.forEach(r => {
+    const blogId = r.dynamodb.Keys.blogId.S;
+    blogIds.add(blogId);
+  });
+
+  // generate the sites of each affected blog
+  const generateSites = [...blogIds].map(generate);
+  await Promise.all(generateSites);
+};
