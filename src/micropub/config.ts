558e9c3bac3e67ab79bebfc940aa18382830b0d5
Matt Moriarity <matt@mattmoriarity.com>
2018-12-11 18:28:45 -0600

    Include supported post types in config
    Fixes #10.

--- src/micropub/config.ts
+++ src/micropub/config.ts
@@ -2,12 +2,32 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 
 interface MicropubConfig {
   "media-endpoint": string;
+  "post-types": PostType[];
+}
+
+interface PostType {
+  type: string;
+  name: string;
 }
 
 export default function config(event: APIGatewayProxyEvent): MicropubConfig {
   const url = 'https://blog-api.mattmoriarity.com/micropub/media';
 
   return {
-    "media-endpoint": url
+    "media-endpoint": url,
+    "post-types": [
+      {
+        type: 'note',
+        name: 'Status Update'
+      },
+      {
+        type: 'article',
+        name: 'Blog Post'
+      },
+      {
+        type: 'photo',
+        name: 'Photo'
+      }
+    ]
   };
 }
