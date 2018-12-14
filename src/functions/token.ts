72602acd47ba167302e54361f6c57d1e1cf5a9b3
Matt Moriarity <matt@mattmoriarity.com>
2018-12-14 07:24:41 -0600

    Create a tokens endpoint for Micropub
    The IndieAuth.com tokens endpoint we were using doesn't support CORS, so
    we couldn't use it from a client-side only SPA client.
    
    This will also let us just check our token in process without a network
    request.

---
+++ src/functions/token.ts
@@ -0,0 +1,44 @@
+import * as httpError from "http-errors";
+import middy from "middy";
+import * as mw from "middy/middlewares";
+import fetch from "node-fetch";
+import * as qs from "querystring";
+
+import { createToken } from "../micropub/auth";
+import { errorHandler, formDataParser } from "../middlewares";
+
+export const create = middy(async (event, context) => {
+  const { code, me, client_id, redirect_uri } = event.body;
+  // TODO this should be discovered from the `me` site
+  const authUrl = 'https://indieauth.com/auth';
+
+  const resp = await fetch(authUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+    body: qs.stringify({ code, me, client_id, redirect_uri })
+  });
+  const body = await resp.json();
+  if (!resp.ok) {
+    throw new httpError.Unauthorized(body.error_description);
+  }
+
+  const token = createToken(body.me, body.scope);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      me: body.me,
+      scope: body.scope,
+      access_token: token,
+      token_type: 'Bearer'
+    })
+  };
+});
+
+create
+  .use(errorHandler())
+  .use(mw.httpHeaderNormalizer())
+  .use(mw.cors())
+  .use(formDataParser());
--- src/micropub/auth.ts
+++ src/micropub/auth.ts
@@ -1,4 +1,8 @@
 import { PolicyDocument } from "aws-lambda";
+import * as jwt from "jsonwebtoken";
+import uuid from "uuid/v4";
+
+const tokenSecret = Buffer.from(process.env.JWT_SECRET, 'base64');
 
 export function createPolicy(allow: boolean, methodArn: string): PolicyDocument {
   return {
@@ -10,4 +14,12 @@ export function createPolicy(allow: boolean, methodArn: string): PolicyDocument 
       Resource: methodArn
     }]
   };
+}
+
+export function createToken(me: string, scope: string): string {
+  return jwt.sign({ scope: scope }, tokenSecret, {
+    issuer: 'https://blog-api.mattmoriarity.com/token',
+    subject: me,
+    jwtid: uuid()
+  });
 }
